241ef38abf9568108ab126214b5aa98d9b3a97b0
Seongheon Park <n30gu1@n30gu1.com>
2022-01-26 16:41:03 +0900

    Initial Commit

---
+++ ConnectedList/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ConnectedList
+//
+//  Created by Seongheon Park on 2022/01/26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
