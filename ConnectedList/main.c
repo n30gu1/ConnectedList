c3b486b47086d18843995857f9ac798debcee0a5
Seongheon Park <n30gu1@n30gu1.com>
2022-01-27 16:19:00 +0900

    Done

--- ConnectedList/main.c
+++ ConnectedList/main.c
@@ -6,9 +6,114 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ListNode {
+    char data[4];
+    struct ListNode* link;
+} listNode;
+
+typedef struct {
+    listNode* head;
+} linkedList_h;
+
+linkedList_h* createLinkedList_h(void) {
+    linkedList_h* L;
+    L = (linkedList_h*)malloc(sizeof(linkedList_h));
+    L -> head = NULL;
+    return L;
+}
+
+void freeLinkedList_h(linkedList_h* L) {
+    listNode* p;
+    while (L -> head != NULL) {
+        p = L -> head;
+        L -> head = L -> head -> link;
+        free(p);
+        p = NULL;
+    }
+}
+
+void printList(linkedList_h* L) {
+    listNode* p;
+    printf("L = (");
+    p = L -> head;
+    
+    while (p != NULL) {
+        printf("%s", p -> data);
+        p = p -> link;
+        if (p != NULL) {
+            printf(", ");
+        }
+    }
+    printf(") \n");
+}
+
+void insertFirstNode(linkedList_h *L, char *x) {
+    listNode* newNode;
+    newNode = (listNode*)malloc(sizeof(listNode));
+    strcpy(newNode -> data, x);
+    newNode -> link = L -> head;
+    L -> head = newNode;
+}
+
+void insertMiddleNode(linkedList_h* L, listNode *pre, char* x) {
+    listNode* newNode;
+    newNode = (listNode*)malloc(sizeof(listNode));
+    strcpy(newNode -> data, x);
+    if (L == NULL) {
+        newNode -> link = NULL;
+        L -> head = newNode;
+    } else if (pre == NULL) {
+        L -> head = newNode;
+    } else {
+        newNode -> link = pre -> link;
+        pre -> link = newNode;
+    }
+}
+
+void insertLastNode(linkedList_h* L, char* x) {
+    listNode* newNode;
+    listNode* temp;
+    newNode = (listNode*)malloc(sizeof(listNode));
+    strcpy(newNode -> data, x);
+    newNode -> link = NULL;
+    if (L -> head == NULL) {
+        L -> head = newNode;
+        return;
+    }
+    
+    temp = L -> head;
+    while (temp -> link != NULL) {
+        temp = temp -> link;
+    }
+    temp -> link = newNode;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    linkedList_h* L;
+    L = createLinkedList_h();
+    printf("1. Create null list\n");
+    printList(L); getchar();
+    
+    printf("2. Insert node [수] to the list\n");
+    insertFirstNode(L, "수");
+    printList(L); getchar();
+    
+    printf("3. Insert node [금] to end of the list\n");
+    insertLastNode(L, "금");
+    printList(L); getchar();
+    
+    printf("4. Insert node [월] to start of the list\n");
+    insertFirstNode(L, "월");
+    printList(L); getchar();
+    
+    printf("5. Make list null by freeing the list space\n");
+    freeLinkedList_h(L);
+    printList(L);
+    
+    getchar();
+    
     return 0;
 }
